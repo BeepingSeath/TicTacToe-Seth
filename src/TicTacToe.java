4cccf5a7a505df43742ecd7a08c85e06ab500c4c
BeepingSeath <Beeping.Seath.DKS@gmail.com>
2022-02-22 15:19:14 +0100

    Guess input

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,2 +1,14 @@
+import javax.swing.*;
+
 public class TicTacToe {
+    public static void main(String[] args) {
+        boolean Win = false;
+
+        if (Win = false) {
+            String Guess = JOptionPane.showInputDialog("Choose a spot" + "\n|1|2|3|" + "\n|4|5|6|" + "\n|7|8|9|");
+            int GuessI = Integer.parseInt(Guess);
+
+
+        }
+    }
 }
